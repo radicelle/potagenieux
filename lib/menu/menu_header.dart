8004f6a9cdf8f49b14de52ebae73db9f25006339
radicelle <emmanuel.breton-belz@outlook.fr>
2022-03-09 19:03:35 +0100

    renaming

--- lib/menu/menu_header.dart
+++ lib/menu/menu_header.dart
@@ -5,20 +5,20 @@ import '../globals.dart' as globals;
 class MenuHeader extends StatelessWidget {
   const MenuHeader({
     Key? key,
-    required this.menuWidth,
-    required this.panelHeight,
+    required this.width,
+    required this.height,
     required this.rightContainerHeight,
   }) : super(key: key);
 
-  final double menuWidth;
-  final double panelHeight;
+  final double width;
+  final double height;
   final double rightContainerHeight;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: menuWidth,
-      height: panelHeight - rightContainerHeight,
+      width: width,
+      height: height,
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Text(
