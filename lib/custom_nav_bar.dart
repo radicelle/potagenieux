56260be2188e21821878f6f0199a480667489453
radicelle <emmanuel.breton-belz@outlook.fr>
2022-04-21 13:47:13 +0200

    add padding and color

--- lib/custom_nav_bar.dart
+++ lib/custom_nav_bar.dart
@@ -13,26 +13,31 @@ class CustomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<MenuItemProvider>(builder: (context, menuItemProvider, __) {
-      return GNav(tabs: [
-        GButton(
-          icon: Icons.home,
-          text: " Accueil",
-          rippleColor: globals.headerTextColor,
-          iconColor: globals.headerTextColor,
-          textColor: globals.headerTextColor,
-          iconActiveColor: globals.headerTextColor,
-          onPressed: () => menuItemProvider.select(ShadingItem.home),
-        ),
-        GButton(
-          icon: Icons.shopping_basket,
-          text: " Produits",
-          iconActiveColor: globals.headerTextColor,
-          rippleColor: globals.headerTextColor,
-          iconColor: globals.headerTextColor,
-          textColor: globals.headerTextColor,
-          onPressed: () => menuItemProvider.select(ShadingItem.products),
-        ),
-      ]);
+      return Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: GNav(
+            rippleColor: globals.backgroundColor,
+            tabActiveBorder:
+                Border.all(color: globals.backgroundColor, width: 1),
+            tabs: [
+              GButton(
+                icon: Icons.home,
+                text: " Accueil",
+                iconColor: globals.headerTextColor,
+                textColor: globals.headerTextColor,
+                iconActiveColor: globals.headerTextColor,
+                onPressed: () => menuItemProvider.select(ShadingItem.home),
+              ),
+              GButton(
+                icon: Icons.shopping_basket,
+                text: " Produits",
+                iconActiveColor: globals.headerTextColor,
+                iconColor: globals.headerTextColor,
+                textColor: globals.headerTextColor,
+                onPressed: () => menuItemProvider.select(ShadingItem.products),
+              ),
+            ]),
+      );
     });
   }
 }
