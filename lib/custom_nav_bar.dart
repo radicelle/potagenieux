6054d9b23f4c2e6d340a7ad3e6ded85c760ceda3
radicelle <emmanuel.breton-belz@outlook.fr>
2022-04-27 23:01:13 +0200

    fix menu index when swapping from large to mobile

--- lib/custom_nav_bar.dart
+++ lib/custom_nav_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:potagenieux/providers/illuminable.dart';
 import 'package:potagenieux/providers/menu_item_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -12,10 +11,12 @@ class CustomNavBar extends StatelessWidget {
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Consumer<MenuItemProvider>(builder: (context, menuItemProvider, __) {
-      return Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: GNav(
+    return Consumer<MenuItemProvider>(
+      builder: (context, menuItemProvider, __) {
+        return Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: GNav(
+            selectedIndex: menuItemProvider.currentIndex(),
             color: globals.headerTextColor,
             gap: 8,
             tabBorderRadius: 30,
@@ -24,31 +25,19 @@ class CustomNavBar extends StatelessWidget {
               globals.backgroundColor,
               globals.backgroundColor.withOpacity(0.5)
             ]),
-            curve: Curves.easeInExpo, // tab animation curves
+            curve: Curves.easeInExpo,
+            // tab animation curves
             duration: const Duration(milliseconds: 600),
             tabs: [
-              GButton(
-                icon: Icons.home,
-                text: "Accueil",
-                onPressed: () => menuItemProvider.select(ShadingItem.home),
-              ),
-              GButton(
-                icon: Icons.portrait,
-                text: "Blog",
-                onPressed: () => menuItemProvider.select(ShadingItem.blog),
-              ),
-              GButton(
-                icon: Icons.fiber_new_sharp,
-                text: "News",
-                onPressed: () => menuItemProvider.select(ShadingItem.news),
-              ),
-              GButton(
-                icon: Icons.shopping_basket,
-                text: "Produits",
-                onPressed: () => menuItemProvider.select(ShadingItem.products),
-              ),
-            ]),
-      );
-    });
+              ...MenuItemProvider.items.entries.map((i) => GButton(
+                    icon: i.value.iconData,
+                    onPressed: () => menuItemProvider.select(i.key),
+                    text: i.value.text,
+                  ))
+            ],
+          ),
+        );
+      },
+    );
   }
 }
--- lib/providers/menu_item_provider.dart
+++ lib/providers/menu_item_provider.dart
@@ -1,7 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:potagenieux/providers/illuminable.dart';
 
 class MenuItemProvider extends Illuminable {
   late ShadingItem selectedItem;
+  static const Map<ShadingItem, MenuItemObject> items = {
+    ShadingItem.home:
+        MenuItemObject(iconData: Icons.home, text: "Accueil", index: 0),
+    ShadingItem.blog:
+        MenuItemObject(iconData: Icons.portrait, text: "Blog", index: 1),
+    ShadingItem.news:
+        MenuItemObject(iconData: Icons.fiber_new_sharp, text: "News", index: 2),
+    ShadingItem.products: MenuItemObject(
+        iconData: Icons.shopping_basket, text: "Produits", index: 3),
+  };
+
+  currentIndex() => items[selectedItem]?.index;
 
   MenuItemProvider() {
     selectedItem = ShadingItem.home;
@@ -10,4 +23,12 @@ class MenuItemProvider extends Illuminable {
     selectedItem = item;
     notifyListeners();
   }
+}
+
+class MenuItemObject {
+  const MenuItemObject(
+      {required this.iconData, required this.index, required this.text});
+  final IconData iconData;
+  final int index;
+  final String text;
 }
