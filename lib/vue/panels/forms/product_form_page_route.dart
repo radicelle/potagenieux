ca4a0ac223411b85cfe3b439b468966a274ebfb1
radicelle <emmanuel.breton-belz@outlook.fr>
2022-05-16 18:27:46 +0200

    ok on android v0

--- lib/vue/panels/forms/product_form_page_route.dart
+++ lib/vue/panels/forms/product_form_page_route.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 class ProductFormPageRoute extends StatelessWidget {
   const ProductFormPageRoute({Key? key}) : super(key: key);
@@ -26,26 +30,56 @@ class AddProductForm extends StatefulWidget {
 }
 
 class _AddProductFormState extends State<AddProductForm> {
-  late CameraController? _controller;
-  late Future<void>? _initializeControllerFuture;
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+  File? _imageFile;
 
   @override
   void initState() {
     super.initState();
     var cameraList = availableCameras();
-    cameraList.then((list) => {
-          _controller = CameraController(
-            list.first,
-            ResolutionPreset.medium,
-          )
-        });
-    //if (_controller == null) _controller = CameraController(, resolutionPreset);
-    // Next, initialize the controller. This returns a Future.
-    _initializeControllerFuture = _controller?.initialize();
+    cameraList
+        .then((list) => {
+              _controller = CameraController(
+                list.first,
+                ResolutionPreset.medium,
+              )
+            })
+        .whenComplete(
+            () => _initializeControllerFuture = _controller.initialize());
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        const Spacer(),
+        PlatformElevatedButton(
+            child: const Text("Shoot"),
+            onPressed: () async {
+              var image = await _controller.takePicture();
+              setState(() {
+                _imageFile = File(image.path);
+              });
+            }),
+        const Spacer(),
+        _imageFile != null
+            ? Container(
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.black)),
+                child: kIsWeb
+                    ? Image.network(_imageFile!.path)
+                    : Image.file(File(_imageFile!.path)),
+              )
+            : const FlutterLogo(),
+        const Spacer()
+      ],
+    );
   }
 }
