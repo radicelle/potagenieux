d11523b63d571085ad32a287880eb6748b15c7fe
manumanmax <emmanuel.breton-belz@eurecia.com>
2022-06-10 17:48:09 +0200

    complete rework of add product page

--- lib/vue/panels/forms/product_form_page_route.dart
+++ lib/vue/panels/forms/product_form_page_route.dart
@@ -1,8 +1,11 @@
+import 'dart:ffi';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:potagenieux/vue/panels/forms/widgets/cam_preview_condensed.dart';
 import 'package:potagenieux/vue/panels/listViews/product_tile.dart';
+import 'package:potagenieux/globals.dart' as globals;
 
 class ProductFormPageRoute extends StatelessWidget {
   const ProductFormPageRoute(this.cameras, {Key? key}) : super(key: key);
@@ -83,16 +86,18 @@ class _AddProductFormState extends State<AddProductForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(globals.menuBackgroundHexa),
       body: LayoutBuilder(builder: (context, constraints) {
         var width = constraints.maxWidth / 1.1;
         var height = width / 1.25;
+        var primaryButtonColor = globals.menuBackgroundColor.withAlpha(100);
         return Column(
           children: [
-            Spacer(
-              flex: 1,
+            const Spacer(
+              flex: 2,
             ),
             Flexible(
-              flex: 6,
+              flex: 10,
               child: ProductTile(
                 desc: "description",
                 width: width,
@@ -114,39 +119,98 @@ class _AddProductFormState extends State<AddProductForm> {
               ),
             ),
             Flexible(
-              flex: 1,
-              child: PlatformSlider(
-                value: _zoom,
-                min: 1.0,
-                max: 4.0,
-                onChanged: (value) => {
-                  setState(
-                    () {
-                      _zoom = value;
-                      _controller.setZoomLevel(_zoom);
-                    },
-                  )
-                },
+              flex: 2,
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Center(child: Text("Zoom")),
+                    Center(
+                      child: PlatformSlider(
+                        material: (_, __) => MaterialSliderData(activeColor: globals.headerTextColor),
+                        value: _zoom,
+                        min: 1.0,
+                        max: 4.0,
+                        onChanged: (value) => {
+                          setState(
+                            () {
+                              _zoom = value;
+                              _controller.setZoomLevel(_zoom);
+                            },
+                          )
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            Row(
-              children: [
-                ...widget.cameras.map((e) => Flexible(
-                    flex: 1,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _initializeControllerFuture = onNewCameraSelected(e);
-                        });
-                      },
-                      child: Column(
-                          children: [const Icon(Icons.camera), Text(e.name)]),
-                    )))
-              ],
+            Flexible(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ...widget.cameras.map(
+                    (e) => Flexible(
+                      flex: 1,
+                      child: Center(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(primary: primaryButtonColor),
+                          onPressed: () {
+                            setState(() {
+                              _initializeControllerFuture =
+                                  onNewCameraSelected(e);
+                            });
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: e.name.cameraInfo(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(
+              flex: 2,
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              flex: 3,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                    primary: primaryButtonColor,
+                    surfaceTintColor: Colors.transparent),
+                onPressed: () {},
+                child: Icon(
+                  Icons.camera,
+                  size: constraints.maxWidth / 5,
+                ),
+              ),
+            ),
+            const Spacer(
+              flex: 2,
             )
           ],
         );
       }),
     );
+  }
+}
+
+extension on String {
+  List<Widget> cameraInfo() {
+    switch (this) {
+      case "0":
+        return [const Icon(Icons.camera_rear), const Text("dos")];
+      case "1":
+        return [const Icon(Icons.camera_front), const Text("selfie")];
+      default:
+        return [];
+    }
   }
 }
